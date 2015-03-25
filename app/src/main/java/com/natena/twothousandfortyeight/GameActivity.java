ac299b8ea797c1c7c41392123fd47ab6a936c864
Natalie Fuller <nataliethefuller@gmail.com>
2015-03-24 22:05:59 -0700

    Initial commit of basic Android project.

---
+++ app/src/main/java/com/natena/twothousandfortyeight/GameActivity.java
@@ -0,0 +1,23 @@
+package com.natena.twothousandfortyeight;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Main Activity class for our game.
+ */
+public class GameActivity extends Activity
+{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onDestroy()
+    {
+        super.onDestroy();
+    }
+}
